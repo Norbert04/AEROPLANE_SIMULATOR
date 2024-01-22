e872e5c06bfcda1d52ce9929d39c697b383c4c47
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2024-01-22 22:01:03 +0000

    made main window class a singleton

--- FLIGHT_SIMULATOR/MainWindow.hpp
+++ FLIGHT_SIMULATOR/MainWindow.hpp
@@ -7,5 +7,15 @@ class MainWindow
 private:
 	HWND hWnd;
 
+	MainWindow(HWND hWnd);
+
+	static MainWindow* window;
+
+public:
+	MainWindow(const MainWindow&) = delete;
+	void operator=(const MainWindow&) = delete;
+
+	static MainWindow* getWindow();
+	static void deleteWindow();
 };
 
