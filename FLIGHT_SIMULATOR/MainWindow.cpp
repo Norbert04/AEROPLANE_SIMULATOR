ddfbf4d524b9d32af383b64daef36ddbf7ea6037
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2024-01-22 22:16:57 +0000

    added method implementations to get and delete window singleton

--- FLIGHT_SIMULATOR/MainWindow.cpp
+++ FLIGHT_SIMULATOR/MainWindow.cpp
@@ -1,3 +1,18 @@
 #include "MainWindow.hpp"
 
+MainWindow* MainWindow::getWindow()
+{
+	if (window == NULL)
+		window = new MainWindow(0); // TODO: create real window
+
+	return window;
+}
+
+void MainWindow::deleteWindow()
+{
+	// TODO: properly close window
+	delete window;
+	window = NULL;
+}
+
 MainWindow* MainWindow::window = NULL;
