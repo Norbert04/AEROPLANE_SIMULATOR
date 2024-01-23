891f6ce9b242e1a28acfc007ca63e14f1085c6f5
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2024-01-23 22:12:32 +0000

    added message loop

--- FLIGHT_SIMULATOR/main.cpp
+++ FLIGHT_SIMULATOR/main.cpp
@@ -9,7 +9,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
 	ShowWindow(MainWindow::getWindow()->getHWnd(), nCmdShow);
 
-
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
 }
