0fc28413cbc65da3affc454b061648b428791105
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2024-01-23 22:09:55 +0000

    now showing window

--- FLIGHT_SIMULATOR/main.cpp
+++ FLIGHT_SIMULATOR/main.cpp
@@ -3,9 +3,13 @@
 #endif // !UNICODE
 
 #include "includes.hpp"
+#include "MainWindow.hpp"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
+	ShowWindow(MainWindow::getWindow()->getHWnd(), nCmdShow);
+
+
 
 	return 0;
 }
