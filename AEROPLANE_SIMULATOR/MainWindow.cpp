fd6c1bc5c5c8dfccd20cbe78867a31bca06fa9b4
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2024-02-03 22:00:59 +0000

    Created the swap chain

--- AEROPLANE_SIMULATOR/MainWindow.cpp
+++ AEROPLANE_SIMULATOR/MainWindow.cpp
@@ -52,6 +52,55 @@ MainWindow::MainWindow()
 
 	device.As(&d3dDevice);
 	deviceContext.As(&d3dDeviceContext);
+
+	// create swap chain
+
+	//the following code (until the comment marking the end) is edited and licensed under the following license:
+	/*The MIT License(MIT)
+	Copyright(c) Microsoft Corporation
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+	associated documentation files(the "Software"), to deal in the Software without restriction,
+	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and /or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions :
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial
+	portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+	NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.OutputWindow = hWnd;
+	swapChainDesc.Windowed = TRUE;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+
+	Microsoft::WRL::ComPtr<IDXGIDevice4> deviceDXGI;
+	deviceDXGI.As(&dxgiDevice);
+
+	Microsoft::WRL::ComPtr<IDXGIFactory> factory;
+	Microsoft::WRL::ComPtr<IDXGIAdapter> adapter;
+
+	if (SUCCEEDED(deviceDXGI->GetAdapter(&adapter)))
+	{
+		adapter->GetParent(IID_PPV_ARGS(&factory));
+
+		factory->CreateSwapChain(
+			d3dDevice.Get(),
+			&swapChainDesc,
+			&dxgiSwapChain
+		);
+	}
+	// from here on the license specified in the license file is used
 }
 
 MainWindow* MainWindow::getWindow()
